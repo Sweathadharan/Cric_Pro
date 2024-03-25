a0d1a03258385fded7ed1e2141be10d7c49b84f9
Sweatha <89176350+Sweathadharan@users.noreply.github.com>
2024-03-25 11:29:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+const logo=document.querySelector('.logo');
+logo.style.display="block";
+const shewagImg = document.getElementById("shewagimg");
+
+const ImgClick=document.getElementById("shewag");
+
+ImgClick.addEventListener("click",function(){
+  
+  shewagImg.style.display="block"
+})
+
+const sachinImg = document.getElementById("sachinimg");
+const Img=document.getElementById("sachin");
+
+Img.addEventListener("click",function(){
+  
+  sachinImg.style.display="block"
+})
+
+const gautamImg = document.getElementById("gautamimg");
+const img=document.getElementById("gautam");
+
+img.addEventListener("click",function(){
+  
+    gautamImg.style.display="block"
+})
+
+const viratImg = document.getElementById("viratimg");
+const image=document.getElementById("virat");
+
+image.addEventListener("click",function(){
+  
+    viratImg.style.display="block"
+})
+
+const dhoniImg = document.getElementById("dhoniimg");
+const images=document.getElementById("dhoni");
+
+images.addEventListener("click",function(){
+  
+    dhoniImg.style.display="block"
+})
+
+const yuvarajImg = document.getElementById("yuvarajimg");
+const imag=document.getElementById("yuvaraj");
+
+imag.addEventListener("click",function(){
+  
+    yuvarajImg.style.display="block"
+})
+
+const sureshImg = document.getElementById("sureshimg");
+const imag1=document.getElementById("suresh");
+
+imag1.addEventListener("click",function(){
+  
+    sureshImg.style.display="block"
+})
+
+const harbajanImg = document.getElementById("harbhajanimg");
+const imag2=document.getElementById("harbajan");
+
+imag2.addEventListener("click",function(){
+  
+    harbajanImg.style.display="block"
+})
+
+const zaheerImg = document.getElementById("zaheerimg");
+const imag3=document.getElementById("zaheer");
+
+imag3.addEventListener("click",function(){
+  
+    zaheerImg.style.display="block"
+})
+
+const manufImg = document.getElementById("munafimg");
+const imag4=document.getElementById("manuf");
+
+imag4.addEventListener("click",function(){
+  
+    manufImg.style.display="block"
+})
+
+const sreeImg = document.getElementById("sreeimg");
+const imag5=document.getElementById("sree");
+
+imag5.addEventListener("click",function(){
+  
+    manufImg.style.display="block"
+})
+
+
+
